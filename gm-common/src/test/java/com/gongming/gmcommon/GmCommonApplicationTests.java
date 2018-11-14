6c0ba8106324261a3e0aff31a5ba34dc1990be23
fugou <46508860@qq.com>
2018-11-14 10:53:14 +0800

    代码更新。

---
+++ gm-common/src/test/java/com/gongming/gmcommon/GmCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gongming.gmcommon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmCommonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
